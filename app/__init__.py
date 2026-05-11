a745af6875d04818df572b3c8a75c0c61e870053
Xerobrin120 <151674035+Xerobrin120@users.noreply.github.com>
2026-05-11 23:39:38 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -7,6 +8,7 @@ from config import Config
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
@@ -14,6 +16,7 @@ def create_app():
     
     db.init_app(app)
     bcrypt.init_app(app)
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     login_manager.login_view = 'login'
     
