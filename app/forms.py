289d585b77e40f6a7f0d88af5f93a0241b57ab23
Xerobrin120 <151674035+Xerobrin120@users.noreply.github.com>
2026-05-11 23:40:29 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,12 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class RegisterForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired(), Length(min=3)])
     password = PasswordField('Пароль', validators=[DataRequired(), Length(min=3)])
+    avatar = FileField('Аватар (jpg, png)', validators=[FileAllowed(['jpg', 'png'], 'Только картинки!')])
     submit = SubmitField('Зарегистрироваться')
 
 class LoginForm(FlaskForm):
